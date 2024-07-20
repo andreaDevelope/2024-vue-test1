3396eb3a3b036decaf1868e368e8508abc11ade0
andreaDevelope <andrea_develope@outlook.it>
2024-07-20 17:52:07 +0200

    crud completa devo risolvere un problema con la edit farm

--- src/views/FarmView.vue
+++ src/views/FarmView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <div  v-if="!newFarmShow" class="swhitch"><!--v-if 1-->
+        <div  v-if="!newFarmShow && !updateFarmShow" class="swhitch"><!--v-if 1-->
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.qGo0IrBjfJ9uFguGHfoM9wAAAA%26pid%3DApi&f=1&ipt=4168865cb3d14fda95ca7359b0ee1e90d7c3d4d572702fdd6dd0e8349fc252de&ipo=images" alt="immagine-fattoria">
             <h1>Farm</h1>
             <button @click="addTestData">test add farmer and farm</button>
@@ -8,6 +8,8 @@
             <ol>
                 <li v-for="farm in farms" :key="farm.id">
                   <div class="li-separetor"><span>NAME: </span>{{ farm.name }}</div> <div class="li-separetor"><span>CITY: </span>{{ farm.city }}</div> 
+                  <div class="li-separetor"><button @click="editFarm(farm.id)">edit farm</button></div>
+                  <div class="li-separetor"><button @click="deleteFarm(farm.id)">delete farm</button></div>
                 </li>
 
             </ol>
@@ -17,13 +19,24 @@
         <div v-if="newFarmShow" class="swhitch"><!-- v-if 1 -->
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.qGo0IrBjfJ9uFguGHfoM9wAAAA%26pid%3DApi&f=1&ipt=4168865cb3d14fda95ca7359b0ee1e90d7c3d4d572702fdd6dd0e8349fc252de&ipo=images" alt="immagine-fattoria">
             <h1>New Farm</h1>
-            <input v-model="newFarmData.name" type="text" placeholder="new name">
-            <input v-model="newFarmData.city" type="text" placeholder="new city">
+            <input v-model="newFarmData.name" type="text" placeholder="name">
+            <input v-model="newFarmData.city" type="text" placeholder="city">
             <div>
                 <button @click="toggleNewFarmShow">cancel</button>
                 <button @click="saveNewFarm">save</button>
             </div>
         </div>
+
+        <!-- cambio schermo legato al bottone edit farm -->
+         <div v-if="updateFarmShow" class="swhitch">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.qGo0IrBjfJ9uFguGHfoM9wAAAA%26pid%3DApi&f=1&ipt=4168865cb3d14fda95ca7359b0ee1e90d7c3d4d572702fdd6dd0e8349fc252de&ipo=images" alt="immagine-fattoria">
+            <h1>Update Farm</h1>
+            <input v-model="newFarmData.name" type="text" placeholder="new name">
+            <input v-model="newFarmData.city" type="text" placeholder="new city">
+            <button @click="toggleUpdateFarmShow">cancel</button>
+            <button @click="updateFarm">save</button>
+
+         </div>
     </div>
 </template>
 
@@ -39,11 +52,13 @@ const farms = ref([])
 
 const newFarmShow = ref(false)
 const updateFarmShow = ref(false)
+const updateFarmData = ref({})
 const newFarmData = ref({
   name: '',
   city: ''
 })
 
+// funzione per leggere le farm richiamata con onMounted a fine pagina
 const updateData = ()=>{
     axios
     .get('http://localhost:8080/api/v1/farms')
@@ -56,6 +71,7 @@ const updateData = ()=>{
     })
 }
 
+// funzione che punta al test add di farmer che aggiunge 4 farmer e 2 farm fake per le prove
 const addTestData = ()=>{
     axios
     .get('http://localhost:8080/api/v1/farmers/test/add')
@@ -65,14 +81,17 @@ const addTestData = ()=>{
     })
 }
 
+// cambio schermo new farm
 const toggleNewFarmShow = ()=>{
     newFarmShow.value = !newFarmShow.value
 }
 
-// const toggleUpdateFarmShow = ()=>{
-//     updateFarmShow.value = !updateFarmShow
-// }
+// cambio schermo edit farm
+const toggleUpdateFarmShow = ()=>{
+    updateFarmShow.value = !updateFarmShow.value
+}
 
+// funzione che punta alla rotta per creare una nuova farm
 const saveNewFarm = ()=>{
     axios
     .post('http://localhost:8080/api/v1/farms', newFarmData.value).then((response)=>{
@@ -88,11 +107,49 @@ const saveNewFarm = ()=>{
     })
 }
 
+// funzione per modificare la farm esistente
+const editFarm = (id) => {
+  for (let x = 0; x < farms.value.length; x++) {
+    const farm = farms.value[x]
+
+    if (farm.id === id) {
+      updateFarmData.value = farm
+      break
+    }
+  }
+  updateFarmShow.value = true
+}
+
+// funzione che crea l oggetto axiosData da usare nel patch dell axios sotto
+const updateFarm = () => {
+  const axiosData = {
+    name: updateFarmData.value.name,
+    city: updateFarmData.value.city
+  }
+
+// funzione che punta alla rotta per la modifica
+  axios
+    .patch('http://localhost:8080/api/v1/farms/' + updateFarmData.value.id, axiosData)
+    .then(() => {
+      updateData()
+      toggleUpdateFarmShow()
+    })
+}
+
+// funione che puntando alla rotta delete ci permette di rimuovere una fattoria
+const deleteFarm = (id) => {
+  axios.delete('http://localhost:8080/api/v1/farms/' + id).then(() => {
+    farms.value = farms.value.filter((farm) => farm.id !== id)
+  })
+}
+
+// l' onMounted che richiama updateData per caricare la lista di fattorie
 onMounted(() => {
   updateData()
 })
 
 </script>
+
 <!-- ---------------------------------STYLE---------------------------------- -->
 <style>
     *{
